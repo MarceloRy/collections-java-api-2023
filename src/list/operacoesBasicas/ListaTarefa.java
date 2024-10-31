bb618728c88d742e30f4c9d3e10b167f6c550d91
Gogeta <marcelocb82@hotmail.com>
2024-10-31 18:33:25 -0300

    faltam 2

---
+++ src/list/operacoesBasicas/ListaTarefa.java
@@ -0,0 +1,38 @@
+package list.operacoesBasicas;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+
+    //atributo
+
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa t : tarefaList) {
+            if (t.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(t);
+            }
+
+        }
+
+
+    }
+}
+
+
+
+
+
+
